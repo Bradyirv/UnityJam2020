652d75c4f6fc4a4ff63343e192f248b647c7875a
daybrayk <braydonajohnson@gmail.com>
2020-10-25 20:08:20 -0400

    Removed trigger deactivation

--- PoliticalEspionage/Assets/Scripts/Interactables/Bell.cs
+++ PoliticalEspionage/Assets/Scripts/Interactables/Bell.cs
@@ -10,24 +10,12 @@ public class Bell : MonoBehaviour, IInteractable
     public AudioSource source;
     public BoxCollider trigger;
 
-    private void Start()
-    {
-        ConfessionManager.onConfessionFinish += ActivateTrigger;
-    }
-
     public void Interact()
     {
         if (AIManager.instance.CalledForNextAI())
         {
-            trigger.gameObject.SetActive(false);
             onBellInteract.TryInvoke();
             source.Play();
-            trigger.enabled = false;
         }
-    }
-
-    public void ActivateTrigger()
-    {
-        trigger.enabled = true;
     }
 }
